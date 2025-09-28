59c8c5cb5558a6390992f0bb08d385f6ed842b26
alifrachmat2002 <aaliplagi@gmail.com>
2025-09-28 11:21:34 +0700

    feat: enhance DataTable with pagination and filtering capabilities

--- components/ui/data-table/data-table.tsx
+++ components/ui/data-table/data-table.tsx
@@ -2,9 +2,15 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
+  VisibilityState,
 } from "@tanstack/react-table"
 
 import {
@@ -15,6 +21,10 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { DataTablePagination } from "./pagination"
+import { useState } from "react"
+import { Input } from "@/components/input"
+import { DataTableViewOptions } from "./data-table-view-options"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -25,59 +35,98 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFiters] = useState<ColumnFiltersState>([]);
+  const [globalFilter, setGlobalFilter] = useState<any>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFiters,
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setGlobalFilter,
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnFilters,
+      globalFilter,
+      columnVisibility
+    }
+
   });
 
   return (
-    <div className="overflow-hidden rounded-md border">
-      <div className="">
-        
+      <div className="space-y-2">
+          <div className="flex items-center py-4">
+              <Input
+                  placeholder="Search..."
+                  value={table.getState().globalFilter}
+                  onChange={(event) =>
+                      table.setGlobalFilter(event.target.value)
+                  }
+                  className="max-w-sm"
+              />
+
+              <DataTableViewOptions table={table}/>
+          </div>
+          <div className="overflow-hidden rounded-md border">
+              <Table>
+                  <TableHeader>
+                      {table.getHeaderGroups().map((headerGroup) => (
+                          <TableRow key={headerGroup.id}>
+                              {headerGroup.headers.map((header) => {
+                                  return (
+                                      <TableHead key={header.id}>
+                                          {header.isPlaceholder
+                                              ? null
+                                              : flexRender(
+                                                    header.column.columnDef
+                                                        .header,
+                                                    header.getContext()
+                                                )}
+                                      </TableHead>
+                                  );
+                              })}
+                          </TableRow>
+                      ))}
+                  </TableHeader>
+                  <TableBody>
+                      {table.getRowModel().rows?.length ? (
+                          table.getRowModel().rows.map((row) => (
+                              <TableRow
+                                  key={row.id}
+                                  data-state={row.getIsSelected() && "selected"}
+                              >
+                                  {row.getVisibleCells().map((cell) => (
+                                      <TableCell key={cell.id}>
+                                          {flexRender(
+                                              cell.column.columnDef.cell,
+                                              cell.getContext()
+                                          )}
+                                      </TableCell>
+                                  ))}
+                              </TableRow>
+                          ))
+                      ) : (
+                          <TableRow>
+                              <TableCell
+                                  colSpan={columns.length}
+                                  className="h-24 text-center"
+                              >
+                                  No results.
+                              </TableCell>
+                          </TableRow>
+                      )}
+                  </TableBody>
+              </Table>
+          </div>
+          <DataTablePagination table={table} />
       </div>
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  )
+  );
 }
--- components/views/Admin/Techstack/Techstack.tsx
+++ components/views/Admin/Techstack/Techstack.tsx
@@ -47,8 +47,8 @@ export default function Techstack() {
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                     </DropdownMenu>
-        )
-        },
+            )
+            },
         }
     ],[])
 
